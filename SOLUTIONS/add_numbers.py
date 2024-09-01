36aa4d8c9a1818e152e36d9273400d55b9ba95b8
code-iddih <idinyamwaya@gmail.com>
2024-09-01 13:03:56 +0300

    Adds the add_numbers Function

---
+++ SOLUTIONS/add_numbers.py
@@ -0,0 +1,14 @@
+# Function: add_numbers (2 points)
+
+#Write a Python function named add_numbers that takes two parameters num1 and num2, and returns the sum of the two numbers.
+
+# Creating the function
+
+def add_numbers( num1, num2 ) :
+    # Calculating the sum
+    sum = num1 + num2
+    # Printing the sum
+    print(f"The Sum of the Two Numbers is : {sum}")
+
+# Calling the Function
+add_numbers(12,45)
