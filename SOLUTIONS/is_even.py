a5c7edd11b761806f9b8f0027a3d25ada52a144d
code-iddih <idinyamwaya@gmail.com>
2024-09-01 13:14:05 +0300

    Adds is_even Function

---
+++ SOLUTIONS/is_even.py
@@ -0,0 +1,15 @@
+# Function: is_even (2 points)
+
+# Write a Python function named is_even that takes a single parameter number and returns True if the number is even, and False otherwise.
+
+# Creating the function
+def is_even( number ):
+    # Checks if the number is EVEN
+    if number % 2 == 0 :
+        print(f"The Number {number} is EVEN.")
+    # if the number is not EVEN then it is ODD
+    else : 
+        print(f"The Number {number} is ODD.")
+
+# Calling the Function
+is_even(0)
