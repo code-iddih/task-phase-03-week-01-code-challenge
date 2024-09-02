20765e1458fa5c32a250652b3802dc730b508549
code-iddih <idinyamwaya@gmail.com>
2024-09-02 08:34:17 +0300

    Adds the reverse_function

---
+++ SOLUTIONS/reverse_string.py
@@ -0,0 +1,15 @@
+# Function: reverse_string (2 points)
+
+# Write a Python function named reverse_string that takes a string text as input and returns the reversed version of that string.
+
+# Creating the Function
+def reverse_string():
+    # Request input from User
+    text = input("Please Enter the String You wish to reverse :  ")
+    # Begin from the end and end at the beginning, one step backwards
+    reversed_text = text[::-1]
+    # Printing the new Text
+    print(f"When the Text '{text}' is reversed, it becomes : {reversed_text} ")
+
+# Calling the Function
+reverse_string()
