fa5cf178f7a2dc41f89ed9544a7b11aced873f74
code-iddih <idinyamwaya@gmail.com>
2024-09-02 13:18:54 +0300

    Adds the calculate_factorial Function

---
+++ SOLUTIONS/calculate_factorial.py
@@ -0,0 +1,23 @@
+# Function: calculate_factorial (2 points)
+
+# Write a Python function named calculate_factorial that takes a non-negative integer n as input and returns the factorial of that number. The factorial of a non-negative integer n is the product of all positive integers less than or equal to n.
+
+# Creating the Function
+def calculate_factorial():
+    # Request input from User
+    n = int(input("Please Enter a Number to Calculate its Factorial! :  "))
+    # Initializing the Factorial variable to 1
+    factorial = 1
+    # Checking if the number is a Positive Integer
+    if n < 0:
+        print(f"Invalid!! Please Enter a Positive Integer.")
+    elif n == 0 or n == 1:
+        print(f"The Factorial of the Number {n} is 1.")
+    else:
+        # iterating through the numbers less thaan it
+        for i in range (2, n +1):
+            # Multiplying by current value
+            factorial *= i
+        print(f"The Factorial of the Number {n} is : {factorial}.")
+
+calculate_factorial()
