27ad0b862f5f36d6faad791611f37cde7fd75770
code-iddih <idinyamwaya@gmail.com>
2024-09-02 16:29:45 +0300

    Adds the sort_by_age Function

---
+++ SOLUTIONS/sort_by_age.py
@@ -0,0 +1,18 @@
+# Sequences: Sort List of Tuples (1 point)
+
+# Given a list of tuples where each tuple contains a name and an age, write a Python function named sort_by_age that sorts the list of tuples by age in ascending order.
+
+# Creaing the Function
+def sort_by_age():
+    # Initializingg a List of tuples
+    details = [(" Eric " , " 40 ") , (" Ayim " , " 28 ") , (" Centrine " , " 20 ") , (" George " , " 33 ") ]
+
+    # Sorting the list of Tuples by Age
+    details.sort(key = lambda x : x[1]) # Since age in index 1 of the tuple
+
+    # Printing the Solutiion
+    for detail in details: # So that the ouutput prints vertically
+        print(detail)
+
+# Calling thhe Function
+sort_by_age()
