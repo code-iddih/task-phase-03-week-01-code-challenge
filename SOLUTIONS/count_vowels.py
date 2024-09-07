ab504d7a7996822e8b9d2812777ff950109c0577
code-iddih <idinyamwaya@gmail.com>
2024-09-07 23:38:57 +0300

    Add Parameter to the Function

--- SOLUTIONS/count_vowels.py
+++ SOLUTIONS/count_vowels.py
@@ -3,15 +3,15 @@
 # Write a Python function named count_vowels that takes a string text as input and returns the count of vowels (a, e, i, o, u) in the string. Ignore case sensitivity.
 
 # Creating the Function
-def count_vowels():
+def count_vowels(text):
     # Request input from User
-    text = input("Please Enter the Text You wish to Count Vowels :  ")
+    # text = input("Please Enter the Text You wish to Count Vowels :  ")
     # The Set of Vowels
-    vowels = {"a" , "e" , "i" , "o" , "u" }
+    vowels = "aeiou"
     # Counting the Vowels
     vowel_count = 0
     # Iterating through the text
-    for vowel in text:
+    for vowel in text.lower():
         if vowel in vowels:
             # Increamenting the count
             vowel_count += 1
@@ -23,4 +23,4 @@ def count_vowels():
     else:
         print(f"The text '{text}' contains no Vowels.")
 
-count_vowels()
+count_vowels("Eric")
